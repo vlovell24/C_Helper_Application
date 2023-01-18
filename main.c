2495d8a0013d844f416e8fff4d63f69227beea5b
vlovell24 <vlovell24@aol.com>
2023-01-18 09:19:35 -0500

    Commit numero uno

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "muhfunctions.h"
+
+
+//--------------------FUNCTION PROTOTYPES--------------------------------
+void mainMenu();
+void libraries();
+void formatSpecifiers();
+//-----------------------------------------------------------------------
+
+//Main Here
+int main(){
+	mainMenu();
+	return 0;
+}
+
+//-------------------------MAIN MENU-------------------------------------
+void mainMenu(){
+	char userInput;
+	printf("\n**********Welcome to the C Helper Program**********\n");
+	printf("____________________________________________________\n\n");
+	printf("    <F> = Format Specifiers  <E> = Exit Program\n");
+	printf("    <L> = Libraries          <W> = GCC/GNU on Windows\n");
+    printf("Enter a character: ");
+    scanf("%c", &userInput);     
+
+    
+    printf("You entered %c.\n",userInput);  
+
+	switch(toupper(userInput)){
+		case 'F':
+			fflush(stdin);
+			formatSpecifiers();
+			break;
+}
+
+   
+} // end of mainMenu function
+
+
+void formatSpecifiers(){
+	printf("Format Specifiers Here\n");
+	mainMenu();
+}
+
+
+void libraries(){
+	printf("Libraries Here");
+}
---
+++ muhfunctions.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void mainMenu();
+void escapeSequences() {
+	printf("Testing this here file.");
+}
