f1ca38dbd7c15c1aea4dbea5fafb058cc5f66014
vlovell24 <vlovell24@aol.com>
2023-01-22 22:40:07 -0500

    adding escape sequences

--- muhfunctions.h
+++ muhfunctions.h
@@ -2,5 +2,11 @@
 
 void mainMenu();
 void escapeSequences() {
-	printf("Testing this here file.");
+	printf("\n***********************ESCAPE SEQUENCES********************\n");
+	printf("______________________________________________________________\n");
+	printf(" \\t -> Horizontal Tab\n");
+	printf(" \\n -> New Line\n");
+	printf(" \\' -> Single Quote");
+
+	mainMenu();
 }
